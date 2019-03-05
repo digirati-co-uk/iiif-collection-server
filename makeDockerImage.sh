825ccf0c9196943f7bc9e0a07b936fe349698023
Adam Meszaros <adam.meszaros@digirati.com>
2019-03-05 16:18:36 +0000

    added commit based tagging for dockerhub

--- makeDockerImage.sh
+++ makeDockerImage.sh
@@ -14,6 +14,9 @@ if [[ "$(docker images -q iiif-collection-server:latest 2> /dev/null)" == "" ]];
   exit 1
 fi
 
+docker tag iiif-collection-server:latest digirati/iiif-collection-server:$TRAVIS_COMMIT
+docker push digirati/iiif-collection-server:$TRAVIS_COMMIT
+
 if [[ "$TRAVIS_BRANCH" = "master" ]] && [[ "$TRAVIS_PULL_REQUEST" = "false" ]]; then
   echo -e "\033[00;32m ===> Pushing to Dockerhub\033[0m\n";
   docker tag iiif-collection-server:latest digirati/iiif-collection-server:latest
