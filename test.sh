5117b3fc0a0b05137de931ae0f7486d05dcf1e18
Adam Meszaros <adam.meszaros@digirati.com>
2019-03-06 19:33:15 +0000

    trying to fix the google compute engine issue

--- test.sh
+++ test.sh
@@ -5,7 +5,8 @@
 source venv/bin/activate
 
 pip install -r requirements.txt -r requirements.test.txt
-
+# Travis-ci isssue: https://github.com/travis-ci/travis-ci/issues/7940
+export BOTO_CONFIG=/dev/null 
 export COLLECTION_SERVER_S3_BUCKET=test-bucket
 export COLLECTION_SERVER_S3_BASE_PREFIX=test-collection
 export COLLECTION_SERVER_S3_ACCESS_KEY=xxxxxxxxxxxxxxxxxxxx
